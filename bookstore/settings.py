0d7fe9c97841dd69942710dc69cf0d2eb585d4dd
AJ Sorbello <ajsorbello@gmail.com>
2024-09-18 13:25:34 -0700

    added localhost settings

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -1,6 +1,12 @@
 import os
 from pathlib import Path
 import dj_database_url
+from dotenv import load_dotenv
+
+# Load environment variables from a .env file if not in production
+if os.environ.get('DJANGO_ENV') != 'production':
+   
+    load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') or os.environ.get('BOOKSTORE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
@@ -73,7 +79,7 @@ DATABASES = {
 
 # If on Heroku, use PostgreSQL
 if os.environ.get('DATABASE_URL'):
-    DATABASES['default'] = dj_database_url.config(conn_max_age=500, ssl_require=True)
+    DATABASES['default'] = dj_database_url.config(conn_max_age=500, default=f'sqlite:///{BASE_DIR / "db.sqlite3"}') 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
